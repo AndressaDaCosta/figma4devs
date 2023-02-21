d810dc75d1dea9b9755de651d0ea6b882f8233de
AndressaDaCosta <AndressaDaCosta@users.noreply.github.com>
2023-02-21 00:15:53 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,29 @@
 /**
  * Do not edit directly
- * Generated on Mon, 20 Feb 2023 19:18:13 GMT
+ * Generated on Tue, 21 Feb 2023 00:15:53 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#50505c",
   "ColorGray800": "#1c1c1f",
   "ColorGray900": "#121214",
-  "ColorPurple500": "#8b5cf6"
+  "ColorPurple400": "#9869ff",
+  "ColorPurple500": "#8b5cf6",
+  "FontLabel": {"fontSize":16,"textDecoration":"none","fontFamily":"Helvetica","fontWeight":400,"fontStyle":"normal","fontStretch":"normal","letterSpacing":0,"lineHeight":19.2,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
+  "TypographyLabelFontSize": 16,
+  "TypographyLabelTextDecoration": "none",
+  "TypographyLabelFontFamily": "Helvetica",
+  "TypographyLabelFontWeight": 400,
+  "TypographyLabelFontStyle": "normal",
+  "TypographyLabelFontStretch": "normal",
+  "TypographyLabelLetterSpacing": 0,
+  "TypographyLabelLineHeight": 19.2,
+  "TypographyLabelParagraphIndent": 0,
+  "TypographyLabelParagraphSpacing": 0,
+  "TypographyLabelTextCase": "none"
 };
